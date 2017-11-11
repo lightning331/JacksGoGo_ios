41926617a6ad6f2a7b16fd892e50ea7d53f19246
Hemin Wang <hm.wang0081@gmail.com>
2017-11-11 14:43:39 -0800

    [backup] services

---
+++ JacksGoGo/Classes/Controllers/Services/JGGServiceMainVC.swift
@@ -0,0 +1,28 @@
+//
+//  JGGServiceMainVC.swift
+//  JacksGoGo
+//
+//  Created by Hemin Wang on 11/11/17.
+//  Copyright © 2017 Hemin Wang. All rights reserved.
+//
+
+import UIKit
+
+class JGGServiceMainVC: JGGTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
